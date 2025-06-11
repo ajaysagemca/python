af7c5f9267c12f59d64c3f88d61dd2a20555fe57
Ajay Sahu <153492158+ajaysagemca@users.noreply.github.com>
2025-06-11 18:23:54 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,13 @@
+a=[1,4,9,16,25,36,49,64,81,100]
+x=25
+e=0
+
+for b in a:
+    if(b==x):
+        #e=e+1
+        print('found',e)
+        e=e+1
+    else:
+        print('not found',e)
+        e=e+1
+    print(b)
